7c8a464c60f4e4862f60f0077e2be5263831398c
Rodrigo Sāmietis <rodrigo.samietis@sk.lvt.lv>
2025-09-02 12:07:12 +0300

    Ieviests cikls

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -3,17 +3,19 @@ import java.util.Random;
 
 public class Programma {
 
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		int skaitlis;
-	
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
 		
-		System.out.println("Uzkrita skaitlis: " + skaitlis);
+		for(int i = 1; i <= reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: " + skaitlis);
+		}
+		
 	}
 	
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(5);
 	}
 
 }
