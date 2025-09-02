d6127f98e8184e77a4a584f797375661ce95b07e
Rodrigo Sāmietis <rodrigo.samietis@sk.lvt.lv>
2025-09-02 12:02:11 +0300

    Izveidota metode kauliņa mešanai

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -3,13 +3,17 @@ import java.util.Random;
 
 public class Programma {
 
-	public static void main(String[] args) {
+	static void mestKaulinu() {
 		int skaitlis;
-		
+	
 		Random rand = new Random();
 		skaitlis = rand.nextInt(6)+1;
 		
 		System.out.println("Uzkrita skaitlis: " + skaitlis);
+	}
+	
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 
 }
