9c0869a2ac009f9c2df7ee2b32945704ca86277f
Rodrigo Sāmietis <rodrigo.samietis@sk.lvt.lv>
2025-09-02 12:16:46 +0300

    Ieviesta metamā kauliņa reižu ievadīšana

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -1,15 +1,23 @@
 package lvt;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Programma {
 
 	public static void main(String[] args) {
-		int skaitlis;
-		
+		int skaitlis, reizes;
+		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
 		
-		System.out.println("Uzkrita skaitlis: " + skaitlis);
+		System.out.println("Ievadiet cik reizes mest kauliņu");
+		reizes = scan.nextInt();
+		
+		for(int i = 0; i <= 1; i++) {
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: " + skaitlis);
+		}
+		
+		scan.close();
 	}
 
 }
